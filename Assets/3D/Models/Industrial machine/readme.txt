df4598a495513154c3e81a55a2fa47e74eee0530
Erikpmcr <erikpmcr@gmail.com>
2025-11-06 16:10:17 -0300

    Versão V1.0.0f turma de 2025 (06-11)

---
+++ Assets/3D/Models/Industrial machine/readme.txt
@@ -0,0 +1,6 @@
+Created by Robert I Rabu © 2018
+The 3d models can be used for commercial or non-commercial use.
+
+22ikigurobert@gmail.com
+
+Thank You for your download.
---
+++ Assets/Script/AtivaTexto.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using TMPro;
+public class AtivaTexto : MonoBehaviour
+{
+    public TextMeshProUGUI texto;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void AtivarTexto() 
+    {
+
+        texto.text = "Criado por Erik";
+
+
+    }
+
+
+
+
+
+
+}
